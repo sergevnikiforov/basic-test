a9d00c896fe07967ba9c576b15d715e1c16f4b18
sergevnikiforov <sergevnikiforov@gmail.com>
2016-12-15 23:51:53 +0200

    Tenth

--- js/main.js
+++ js/main.js
@@ -10,23 +10,23 @@
          var elems = document.querySelectorAll("[data-p]");
          console.log(this);
          for (var i = 0; i < elems.length; i++) {
-             //  elems[i].setAttribute('style', 'color: red');
-             //  elems[i].classList.add('show');
-             //alert(elems[i].style.color + " i = " + i);
-             if (this.getAttribute("data-btn") == "p" + i) {
-                 alert(1);
-                 elems[i].setAttribute('style', 'color: green');
-                 elems[i].classList.add('show');
+             elems[i].setAttribute('style', 'color: red');
+             elems[i].classList.add('show');
+             //alert(this.getAttribute("data-btn"));
+             if (this.getAttribute("data-btn") == "a" + i) {
+                 //  alert(1);
+                 //  elems[i].setAttribute('style', 'color: green');
+                 //  elems[i].classList.add('show');
                  elems[i].classList.remove("hidden");
              } else if (this.getAttribute("data-btn") != elems[i].getAttribute("data-p")) {
-                 alert(2);
-                 elems[i].setAttribute('style', 'color: yellow');
-                 elems[i].classList.add('show');
+                 // alert(2);
+                 //  elems[i].setAttribute('style', 'color: yellow');
+                 //  elems[i].classList.add('show');
                  elems[i].classList.add("hidden");
              } else {
-                 alert(3);
-                 elems[i].setAttribute('style', 'color: purple');
-                 elems[i].classList.add('show');
+                 //alert(3);
+                 //  elems[i].setAttribute('style', 'color: purple');
+                 //  elems[i].classList.add('show');
                  elems[i].classList.remove("hidden");
              }
          }
