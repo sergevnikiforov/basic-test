654ced11dd0713277169f8019c7bb2b16f1d4418
sergevnikiforov <sergevnikiforov@gmail.com>
2016-12-15 22:42:29 +0200

    Eights

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@
 
      var btn = document.getElementsByClassName('btn');
 
-
      for (var i = 0; i < btn.length; i++) {
          btn[i].addEventListener("click", DeleteShow);
      }
- }
+
+     function DeleteShow() {
+         var elems = document.querySelectorAll("[data-p]");
+         console.log(this);
+         for (var i = 0; i < elems.length; i++) {
+
+             if (this.getAttribute("data-btn") == "p" + toString(i)) {
+                 elems[i].classList.remove("hidden");
+             } else if (this.getAttribute("data-btn") != elems[i].getAttribute("data-p")) {
+                 elems[i].classList.add("hidden");
+             } else {
+                 elems[i].classList.remove("hidden");
+             }
+         }
+     }
+ };
