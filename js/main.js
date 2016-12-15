479e0c0fea62b9caad05da434c8bbcc85b9f95ea
sergevnikiforov <sergevnikiforov@gmail.com>
2016-12-15 22:37:18 +0200

    Seventh

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-//JS
+ window.onload = function() {
+
+     var btn = document.getElementsByClassName('btn');
+
+
+     for (var i = 0; i < btn.length; i++) {
+         btn[i].addEventListener("click", DeleteShow);
+     }
+ }
